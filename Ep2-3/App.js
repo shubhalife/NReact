e2ba707255ce483e84b22f202feb74be47ddbff7
ecgcdevops <ecgcdevops@cdac.in>
2024-04-15 01:51:25 +0530

    ep2-3

--- Ep2-3/App.js
+++ Ep2-3/App.js
@@ -1,8 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const parent = <h1>Ignite your App!!</h1>
+const heading1 = <h1>Ignite your App!!</h1>;
 
-root.render(parent);
+const Parent = () => (
+  <div id="parent">
+    <Heading2 />
+    <Heading3 />
+  </div>
+);
+
+const num = 1000;
+
+const Heading2 = () => (
+  <div>
+    <h2>{num}</h2>
+    <h2>This is functional component</h2>
+  </div>
+);
+
+const Heading3 = () => {
+  return <h2>This is functional1 component</h2>;
+};
+
+root.render(<Parent />);
