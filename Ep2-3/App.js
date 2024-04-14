10d9b79978a9aafad17309f29f3d428203ca3a00
ecgcdevops <ecgcdevops@cdac.in>
2024-04-15 00:47:46 +0530

    ep2-3

--- Ep2-3/App.js
+++ Ep2-3/App.js
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom/client";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const parent = React.createElement("div",{id: "parent"},"Ignite your APP ");
+const parent = <h1>Ignite your App!!</h1>
 
 root.render(parent);
