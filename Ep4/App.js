fd0a7638a71cb15043636216b99c1b62ecd90202
ecgcdevops <ecgcdevops@cdac.in>
2024-04-16 23:33:16 +0530

    building app ep4

---
+++ Ep4/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//App component
+const App = () => {
+  return <div className="app">APP</div>;
+};
+
+root.render(<App />);
---
+++ Ep4/README.md
@@ -0,0 +1,18 @@
+Header
+-logo
+-about
+-cart
+
+body
+-cards
+--img
+--restaurant name
+--dish name
+--price
+--rating
+--delivery time
+
+footer
+-contact us
+-address
+-copyright
