c4d6e510ae1dfda6ac2514b92ad4af74baa16c7c
ecgcdevops <ecgcdevops@cdac.in>
2024-05-31 15:34:15 +0530

    error.js dog img added

--- Ep4/src/components/Error.js
+++ Ep4/src/components/Error.js
@@ -1,14 +1,32 @@
+import { useEffect, useState } from "react";
 import { useRouteError } from "react-router-dom";
 
 const Error = () => {
+  const [dog, setDog] = useState([]);
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch("https://dog.ceo/api/breeds/image/random");
+
+    const json = await data.json();
+
+    const dogimg = json?.message;
+
+    setDog(dogimg);
+  };
+
   const err = useRouteError();
   return (
     <div className="jumbotron jumbotron-fluid">
-      <div className="container">
+      <div className="error-container">
         <h1 className="display-6">Oops something went wrong</h1>
-        <p className="lead">
+        {/* <p className="lead">
           {err.status} : {err.statusText}
-        </p>
+        </p> */}
+        <img className="dogimg" src={dog} />
+        {/* <img className="dogimg" src={"https://http.cat/" + err.status} /> */}
       </div>
     </div>
   );
